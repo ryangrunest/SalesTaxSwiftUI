9da8a8982f7214ead2bd8b7037da6735452de2d2
Ryan Grunest <ryangrunest@geeko>
2021-09-24 12:55:59 -0700

    initial commit

---
+++ SalesTaxSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalesTaxSwiftUI
+//
+//  Created by Ryan Grunest on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SalesTaxSwiftUI/SalesTaxSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTaxSwiftUIApp.swift
+//  SalesTaxSwiftUI
+//
+//  Created by Ryan Grunest on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SalesTaxSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
